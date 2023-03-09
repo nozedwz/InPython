c674274d5636efb69d91a52fb51a9392a8904f42
Kevyn Oliveira <kevyn_oliveira2020@hotmail.com>
2023-03-09 16:42:32 -0300

    Study 1.0

---
+++ exercicios/ex_6.py
@@ -0,0 +1,9 @@
+import math
+n1 = int(input('digite um número: '))
+n2 = n1*2
+n3 = n1*3
+print(f"""Número selecionado: {n1}
+Dobro do número selecionado: {n2}
+Triplo do número selecionado: {n3}
+Raiz quadrada do número selecionado {math.sqrt(n1):.0f}""")
+#pergunte um número e diga o dobro, o triplo e a raiz quadrada dele
---
+++ exercicios/ex_7.py
@@ -0,0 +1,5 @@
+n1 = int(input('Nota 1° bimestre: '))
+n2 = int(input('Nota 2° bimestre: '))
+n3 = (n1+n2)/2
+print(f'Média: {(n3):.1f}')
+#média de um aluno, 2 bimestres

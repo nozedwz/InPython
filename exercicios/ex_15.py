7d48c1726f186d0822bfa4d1ab1e37ccf1d00c51
Kevyn Oliveira <kevyn_oliveira2020@hotmail.com>
2024-02-01 19:42:05 -0300

    Update ex_15.py

---
+++ exercicios/ex_15.py
@@ -0,0 +1,8 @@
+a = int(input('Digite o número da tabuada a ser mostrada: '))
+b = int(input('Digite o número final da tabuada: '))
+x = a * c
+print ('===============================================================')
+print (f'TABUADA DO NÚMERO{a}')
+for c in range (0,b):
+    print(f'{a} x {c} = {x}')
+print("fim")
